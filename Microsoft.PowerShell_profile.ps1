35e54d7e7e5a9d4d3353e0cd90bc551faa217d64
Konrad Masalski <konradmas@gmail.com>
2022-07-16 16:08:35 +0200

    init

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,14 @@
+#https://www.hanselman.com/blog/adding-predictive-intellisense-to-my-windows-terminal-powershell-prompt-with-psreadline
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
+
+
+Import-Module Terminal-Icons
+Import-Module posh-git
+oh-my-posh --init --shell pwsh --config $env:POSH_THEMES_PATH/jandedobbeleer.omp.json | Invoke-Expression
+
+
 # autostart docker (not needed in W11)
 # https://stackoverflow.com/a/65814529/1219811
 wsl.exe -u root -e sh -c "service docker status || service docker start"
--- windows_kickoff.ps1
+++ windows_kickoff.ps1
@@ -2,13 +2,19 @@
 # in powershell admin
 wsl --install
 
-
 # https://docs.microsoft.com/en-us/windows/wsl/setup/environment
 
+
+# todo nerd fonts
+# https://github.com/ryanoasis/nerd-fonts
+# cd nerd-fonts
+./install.ps1 CascadiaCode
 
 # shell customization
 Install-Module -Name Terminal-Icons -Repository PSGallery
 Install-Module -Name PowerShellGet -Force
 Install-Module PSReadLine -AllowPrerelease -Force
-Install-Module oh-my-posh -Scope AllUsers
-Import-Module oh-my-posh
+
+# OhMyPosh
+winget install JanDeDobbeleer.OhMyPosh -s winget
+winget import winget.json
